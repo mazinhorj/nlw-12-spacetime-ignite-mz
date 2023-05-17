dd75bd55115c0d198ae3360c63b2ea531286f266
mazinhorj <adm.osmarsilva@gmail.com>
2023-05-17 16:07:04 -0300

    favicon as icon & tailwind font/color config

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,17 +1,29 @@
-import { Inter } from 'next/font/google'
+// importação de fontes do google fonts diretamente no next
+import { Bai_Jamjuree as BaiJam, Roboto_Flex as Roboto } from 'next/font/google'
 import { ReactNode } from 'react'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+// configurando as variáveis de fontes do projeto
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const baiJam = BaiJam({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-baijam',
+})
 
+// metadados SEO
 export const metadata = {
   title: 'Spacetime',
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
+    // alterei a lingua padrão da aplicação web
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      {/* tailwind configurado, segue o baile... */}
+      <body className={`font-sans ${roboto.variable} ${baiJam.variable}`}>
+        {children}
+      </body>
     </html>
   )
 }
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
       <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+      <h2 className="font-alt text-2xl font-bold">Nome:</h2>
     </div>
   )
 }
