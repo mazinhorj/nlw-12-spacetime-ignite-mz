ff28b26ae86fcda9393fedd24db93f7879c0fd44
mazinhorj <65859903+mazinhorj@users.noreply.github.com>
2023-05-18 23:02:38 -0300

    mobile avançando

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,68 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import { styled } from 'nativewind'
+import blurBg from './src/assets/bg-blur.png'
+import NlwLogo from './src/assets/nlw-spacetime-logo.svg'
+import Stripes from './src/assets/stripes.svg'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-slate-900">
-      <Text className="text-center text-5xl font-bold text-zinc-100">
-        Are you ready for Spacetime?
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center justify-center bg-gray-900 px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NlwLogo />
+
+        <Text className="space-y-2 text-center">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text>{'\n'}</Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </Text>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 🤍 no NLW da Rocketseat
+        <Text>{'\n'}</Text>
+        Mazinho - 2023
       </Text>
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
